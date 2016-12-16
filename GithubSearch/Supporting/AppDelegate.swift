e8a95239d4b1c8987e7c55b0770920d2feb72799
Er <erisonve@gmail.com>
2016-12-16 18:39:42 +0100

    Setup basic project structure.

---
+++ GithubSearch/Supporting/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  GithubSearch
+//
+//  Created by Er on 12/16/16.
+//  Copyright © 2016 NativeSolutions. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '9.0'
+
+target 'GithubSearch' do
+  use_frameworks!
+  
+end
