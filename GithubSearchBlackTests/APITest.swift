fc260684741adaad31708d2d144558560ca6a882
B4sht4 <erisonve@gmail.com>
2017-02-03 13:57:22 +0100

    Add travis CI config file

---
+++ GithubSearchBlackTests/APITest.swift
@@ -0,0 +1,14 @@
+//
+//  APITest.swift
+//  GithubSearch
+//
+//  Created by Er on 12/23/16.
+//  Copyright © 2016 NativeSolutions. All rights reserved.
+//
+
+import XCTest
+//@testable import UserApi
+
+final class APITest: XCTestCase {
+    
+}
